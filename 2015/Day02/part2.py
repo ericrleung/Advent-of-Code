bd56ec140678a2464e785603a17d5c1f0d145974
Eric Leung <eric.leung.iii@gmail.com>
2025-12-03 22:00:52 -0500

    day 2 2015.

---
+++ 2015/Day02/part2.py
@@ -0,0 +1,10 @@
+with open('input.txt', 'r') as file:
+    lines = file.readlines()
+
+ans = 0
+
+for line in lines:
+    d = line.strip().split("x")
+    d = [int(x) for x in d]
+    ans += int(2 * d[0] + 2 * d[1] + 2 * d[2] - (2 * max(d[0], d[1], d[2]))) + d[0] * d[1] * d[2]
+print(ans)
